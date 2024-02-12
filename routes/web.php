9fa7c65487983c7ce6f037fda96d3c517a0ef5be
GR1GOR1 <MVPROGS@yandex.ru>
2024-02-12 22:53:08 +0300

    try hard

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use App\Http\Controllers\Posts;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Обратить внимание на порядок, иначе /create ловит как id поста и тогда полчаем 404
+Route::get('/posts', [ Posts::class, 'index']);
+Route::get('/posts/create', [ Posts::class, 'create']);
+Route::get('/posts/{id}', [ Posts::class, 'show'])->name('posts.show');
+Route::post('/posts', [ Posts::class, 'store']);
+Route::get('/posts/{id}/edit', [ Posts::class, 'edit']);
+Route::put('/posts/{id}', [ Posts::class, 'update'])->name('posts.update');
